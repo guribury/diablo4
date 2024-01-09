d62cc72b9f93094ccf531e66141acd60a9fc8342
guribury <guribury@naver.com>
2024-01-09 16:52:13 +0900

    스크롤 작업중

--- js/diablo4.js
+++ js/diablo4.js
@@ -1,3 +1,4 @@
+// Swiper
 const swiper = new Swiper('.mySwiper', {
   pagination: {
     el: ".swiper-pagination",
@@ -12,4 +13,16 @@ const swiper = new Swiper('.mySwiper', {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   }
+});
+
+// ScrollMagic
+const spyEls = document.querySelectorAll('section.scroll-spy')
+spyEls.forEach(function(spyEl){
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl,
+      triggerHook: .7
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
 });
